86af550e716fe985361fcaf02c8573178a55c867
Qi Song <alexsong@vt.edu>
2019-05-11 17:01:36 -0400

    Change version info to 1.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ConSReg",
-    version="1.0.7",
+    version="1.0.8",
     author="Qi Song",
     author_email="alexsong@vt.edu",
     description="condition-specific regulation",
